625b7c603d30b90c2879dc698967825885e33f47
Matthew Webb <mwebb1975@gmail.com>
2021-11-25 08:37:35 -0500

    Initial plugin creation

---
+++ ooe-test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * OOE Plugin Test Using Composer
+ * 
+ * @package         WordPress
+ * @author          Matthew Webb
+ * 
+ * @wordpress-plugin
+ * Plugin Name: OOE Plugin Test Using Composer
+ * Description: Testing the creation of an inhouse created plugin and installing using composer
+ * Version: 0.1.0
+ * Author: Matthew Webb
+ */
